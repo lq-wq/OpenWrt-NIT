18fd459324452439bd4c373c0ac21e843bf8a317
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-03-18 20:39:51 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -7,8 +7,10 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 opentomcat 主题
+# 3-添加主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+sed -i 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 
 # 4-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
@@ -16,3 +18,10 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加其它插件
+git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
+git clone -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp 
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
